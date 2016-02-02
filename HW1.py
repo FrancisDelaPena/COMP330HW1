b39859d77cb6e6b2d78cb97c99245c45b9bd8792
TONO-SAMA <jdelapena@luc.edu>
2016-02-02 11:15:13 -0600

    Added basics, working on testing today

---
+++ HW1.py
@@ -0,0 +1,10 @@
+#HW1 for COMP 330
+import unittest
+
+def message():
+    tweet = "When @twotribes go to war, a #point is all you can score."
+    #reference to the example message.  I know 80s music too!
+
+
+
+
